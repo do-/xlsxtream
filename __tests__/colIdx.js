014776c5db478c5df6b1cafccc99e24b1c8707b1
Dmitry Ovsyanko <do_@pochta.ru>
2025-02-16 11:07:03 +0300

    colIdx +digits, Cell.index optimization

--- __tests__/colIdx.js
+++ __tests__/colIdx.js
@@ -2,10 +2,10 @@ const {colIdx} = require ('../lib/Utils.js')
 
 test ('basic', async () => {
 
-	expect (colIdx ('A')).toBe (1)
+	expect (colIdx ('A4')).toBe (1)
 	expect (colIdx ('T')).toBe (20)
-	expect (colIdx ('Z')).toBe (26)
-	expect (colIdx ('AA')).toBe (27)
+	expect (colIdx ('Z11')).toBe (26)
+	expect (colIdx ('AA4')).toBe (27)
 	expect (colIdx ('AQ')).toBe (43)
 	expect (colIdx ('BW')).toBe (75)
 	expect (colIdx ('DF')).toBe (110)
