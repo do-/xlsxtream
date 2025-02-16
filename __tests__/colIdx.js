64b8370079c07d2c3afbaaf2d30d255025380e0e
Dmitry Ovsyanko <do_@pochta.ru>
2025-02-16 11:26:06 +0300

    colIdx -> a1ToColIndex

--- __tests__/colIdx.js
+++ __tests__/colIdx.js
@@ -1,14 +1,14 @@
-const {colIdx} = require ('../lib/Utils.js')
+const {a1ToColIndex} = require ('../lib/Utils.js')
 
 test ('basic', async () => {
 
-	expect (colIdx ('A4')).toBe (1)
-	expect (colIdx ('T')).toBe (20)
-	expect (colIdx ('Z11')).toBe (26)
-	expect (colIdx ('AA4')).toBe (27)
-	expect (colIdx ('AQ')).toBe (43)
-	expect (colIdx ('BW')).toBe (75)
-	expect (colIdx ('DF')).toBe (110)
-	expect (colIdx ('AAA')).toBe (703)
+	expect (a1ToColIndex ('A4')).toBe (1)
+	expect (a1ToColIndex ('T')).toBe (20)
+	expect (a1ToColIndex ('Z11')).toBe (26)
+	expect (a1ToColIndex ('AA4')).toBe (27)
+	expect (a1ToColIndex ('AQ')).toBe (43)
+	expect (a1ToColIndex ('BW')).toBe (75)
+	expect (a1ToColIndex ('DF')).toBe (110)
+	expect (a1ToColIndex ('AAA')).toBe (703)
  	
 })
