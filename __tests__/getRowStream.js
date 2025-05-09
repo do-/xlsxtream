dbb7af2c44f6614805d392503eb5cf264e511a36
Dmitry Ovsyanko <do_@pochta.ru>
2025-05-09 14:46:57 +0300

    xml-toolkit++

--- __tests__/getRowStream.js
+++ __tests__/getRowStream.js
@@ -12,3 +12,26 @@ test ('2_____with_chart', async () => {
 		
 })
 
+test ('err', async () => {
+
+	const wb = await xlsx.open ('__data__/err.xlsx')
+
+    const rows = await wb.sheets [0].getRowStream ()                        
+
+    ROWS: for await (const {index, cells} of rows) if (index === 4) {
+
+		for (const cell of cells) if (cell.index === 2) {
+
+			const v = cell.valueOf ()
+
+			expect (parseFloat (v) === 1946003509).toBe (true)
+
+			expect (v == 1946003509).toBe (true)
+
+			expect (v).toBe ('1.946003509E9')
+
+		}
+
+	}
+		
+})
